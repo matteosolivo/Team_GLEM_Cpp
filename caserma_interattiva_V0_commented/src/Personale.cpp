aad4202c321e2b760d4f3741baae048c933beb98
giovannamasotti <giovanna.masotti@edu.unife.it>
2025-12-02 10:55:50 +0100

    commenti in Personale.cpp
    Added getters and setters for Personale class.

--- caserma_interattiva_V0_commented/src/Personale.cpp
+++ caserma_interattiva_V0_commented/src/Personale.cpp
@@ -3,12 +3,16 @@
 Personale::Personale(int id, const std::string& nome, Grado grado)
     : id(id), nome(nome), grado(grado), disponibile(true) {}
 
+//IMPLEMENTAZIONE DEI GETTERS
 int Personale::getId() const { return id; }
 std::string Personale::getNome() const { return nome; }
 Grado Personale::getGrado() const { return grado; }
 bool Personale::isDisponibile() const { return disponibile; }
+
+//IMPLEMENTAZIONE DEI SETTERS
 void Personale::setDisponibile(bool disp) { disponibile = disp; }
 
+//funzione che mette in relazione enum grado e Stringa che rappresenta il grado
 std::string Personale::gradoToString() const {
     switch (grado) {
         case Grado::SOLDATO: return "Soldato";
